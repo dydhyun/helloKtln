7820836f4f7a14860ad10bbe6ba6c09e0da70218
dydhyun <dydgus625@naver.com>
2025-02-08 13:55:26 +0900

    feat: service 단 예외처리 -> dto 에서 처리

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework:spring-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
 kotlin {
--- src/main/kotlin/com/example/helloKtln/blog/dto/BlogDto.kt
+++ src/main/kotlin/com/example/helloKtln/blog/dto/BlogDto.kt
@@ -1,8 +1,29 @@
 package com.example.helloKtln.blog.dto
 
+import com.example.helloKtln.core.anotation.ValidEnum
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
 data class BlogDto (
-    val query: String,
-    val sort: String,
-    val page: Int,
-    val size: Int
-    )
+    @field:NotBlank(message = "query parameter required")
+    val query: String?,
+
+    @field:NotBlank(message = "sort parameter required")
+    @field:ValidEnum(enumClass = EnumSort::class, message = "sort parameter one of ACCURACY and RECENCY")
+    val sort: String?,
+
+    @field:NotNull(message = "page parameter required")
+    @field:Max(50, message = "page is more then max")
+    @field:Min(1, message = "page is less then min")
+    val page: Int?,
+
+    @field:NotNull(message = "size parameter required")
+    val size: Int?
+) {
+    private enum class EnumSort{
+        ACCURACY,
+        RECENCY
+    }
+}
