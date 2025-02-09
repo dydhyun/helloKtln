a7e72aef1adfe1fc67a2baac512b08dc6e897a88
dydhyun <dydgus625@naver.com>
2025-02-09 23:24:21 +0900

    feat: thymeleaf dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework:spring-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 }
 
 kotlin {
