3e4fe32d2be7d6d71d0a2cbf71193c8698559081
dydhyun <dydgus625@naver.com>
2025-02-10 00:53:12 +0900

    feat: signup RequestMapping 을 활용한 Post, Get mapping

--- src/main/kotlin/com/example/helloKtln/blog/controller/FormHtmlController.kt
+++ src/main/kotlin/com/example/helloKtln/blog/controller/FormHtmlController.kt
@@ -1,16 +1,30 @@
 package com.example.helloKtln.blog.controller
 
+import com.example.helloKtln.blog.dto.User
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 
 @Controller
 class FormHtmlController {
 
-    @GetMapping("/form","/form/{userId}")
-    fun index(@PathVariable("userId")userId: Long?, model: Model):String{
-        val user = com.example.helloKtln.blog.dto.User(null, null, null)
+    @RequestMapping("/form","/form/{userId}", method = [RequestMethod.GET, RequestMethod.POST])
+    fun form(@PathVariable("userId")userId: Long?, user: User?, model: Model):String{
+
+        user?.let{
+            println(user)
+
+            val initUser = User()
+
+            model.addAttribute("user", initUser)
+
+            return "form/signup"
+        }
+
+        val user = User(null, null, null)
 
         if (userId != null){
             user.name = "hello"
@@ -19,6 +33,7 @@ class FormHtmlController {
         }
 
         model.addAttribute("user", user)
+
         return "form/signup"
     }
 
