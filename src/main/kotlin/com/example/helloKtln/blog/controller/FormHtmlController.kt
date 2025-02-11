6374d96e49dddf416966e6cd9e0facf8b9f48ed7
dydhyun <dydgus625@naver.com>
2025-02-11 19:07:30 +0900

    feat: post 요청 메서드 분리

--- src/main/kotlin/com/example/helloKtln/blog/controller/FormHtmlController.kt
+++ src/main/kotlin/com/example/helloKtln/blog/controller/FormHtmlController.kt
@@ -3,12 +3,15 @@ package com.example.helloKtln.blog.controller
 import com.example.helloKtln.blog.dto.User
 import com.example.helloKtln.blog.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class FormHtmlController {
@@ -16,29 +19,30 @@ class FormHtmlController {
     @Autowired
     lateinit var userRepository: UserRepository
 
-    @RequestMapping("/form","/form/{userId}", method = [RequestMethod.GET, RequestMethod.POST])
-    fun form(@PathVariable("userId")userId: Long?, user: User?, model: Model):String{
+    @RequestMapping("/form", "/form/{userId}", method = [RequestMethod.GET])
+    fun form(@PathVariable("userId") userId: Long?, user: User?, model: Model): String {
 
-        user?.let{
-            userRepository.save(it)
-//            userRepository.save(user)
-
-            val initUser = User()
-
-            model.addAttribute("user", initUser)
-
-            return "form/signup"
-        }
-
-        val user = User(null, null, null)
-
-        if (userId != null){
-            user.name = "hello"
-            user.email = "hello@kotlin.com"
-            user.password = "helloPw!"
+        val user = if (userId == null) {
+            User(null, null, null, null)
+        } else{
+            userRepository.findByIdOrNull(userId)
         }
 
         model.addAttribute("user", user)
+
+        return "form/signup"
+    }
+
+    @PostMapping("/form")
+    fun postForm(user: User?, model: Model): String {
+
+        user?.let {
+            userRepository.save(it)
+//            userRepository.save(user)
+        }
+        val initUser = User()
+
+        model.addAttribute("user", initUser)
 
         return "form/signup"
     }
