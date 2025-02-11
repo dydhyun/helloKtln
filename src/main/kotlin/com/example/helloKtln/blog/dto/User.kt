a5f192daf0a57c3ec17b883fd07a75494fde1b95
dydhyun <dydgus625@naver.com>
2025-02-11 19:11:03 +0900

    feat: user Dto 를 userDetail 의 구현체로 업데이트

--- src/main/kotlin/com/example/helloKtln/blog/dto/User.kt
+++ src/main/kotlin/com/example/helloKtln/blog/dto/User.kt
@@ -1,9 +1,13 @@
 package com.example.helloKtln.blog.dto
 
+import com.example.helloKtln.blog.domain.ROLE_TYPE
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 
 @Entity
 data class User(
@@ -13,4 +17,31 @@ data class User(
     var name: String? = null,
     var email: String? = null,
     var password: String? = null
-)
+) : UserDetails{
+    var role = ROLE_TYPE.MEMBER
+
+    override fun getUsername(): String = name!!
+
+    override fun getPassword(): String = password!!
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        val authorities = mutableListOf<GrantedAuthority>()
+
+        when(role){
+            ROLE_TYPE.ADMIN -> authorities.add(SimpleGrantedAuthority("ROLE_ADMIN"))
+            ROLE_TYPE.MEMBER -> authorities.add(SimpleGrantedAuthority("ROLE_MEMBER"))
+        }
+
+        return authorities
+    }
+
+
+}
