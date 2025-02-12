2c3a50a4b0bb784c72bd866dcb39f9a0d4487e47
dydhyun <dydgus625@naver.com>
2025-02-12 20:21:52 +0900

    feat: BCrypt password 암호화저장 추가

--- src/main/kotlin/com/example/helloKtln/blog/controller/FormController.kt
+++ src/main/kotlin/com/example/helloKtln/blog/controller/FormController.kt
@@ -7,15 +7,13 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-class FormHtmlController {
+class FormController {
 
     @Autowired
     lateinit var userRepository: UserRepository
