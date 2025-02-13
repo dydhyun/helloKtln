73eeea966708a2d147aa44a157ffa968b821b9b2
dydhyun <dydgus625@naver.com>
2025-02-13 17:27:44 +0900

    feat: BlogController 블로그 검색결과 json 형식으로 전달

--- src/main/kotlin/com/example/helloKtln/blog/controller/BlogController.kt
+++ src/main/kotlin/com/example/helloKtln/blog/controller/BlogController.kt
@@ -3,15 +3,14 @@ package com.example.helloKtln.blog.controller
 import com.example.helloKtln.blog.dto.BlogDto
 import com.example.helloKtln.blog.entity.Wordcount
 import com.example.helloKtln.blog.service.BlogService
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 @RequestMapping("/api/blog")
 @RestController
@@ -27,24 +26,19 @@ class BlogController(
     @GetMapping("/rank")
     fun searchWordRank(): List<Wordcount> = blogService.searchWordRank()
 
-    //    @PostMapping("/search")
-//    fun searchBlog(@RequestBody query: String): String? {
-//        val blogDto = BlogDto(query, "RECENCY", 1, 1)
-//        println("검색 키워드: ${blogDto.query}")
-//
-//        // 검색 결과 처리 (예제에서는 단순 메시지)
-//        val searchResult = blogService.searchKakao(blogDto)
-//        println("검색 성공: $searchResult")
-//
-//        return searchResult
-//    }
     @PostMapping("/search")
-    fun searchBlog(@RequestBody query: String): ResponseEntity<Map<String, String?>> {
-        val blogDto = BlogDto(query, "RECENCY", 1, 1)
+    fun searchBlog(@RequestBody query: String): ResponseEntity<Map<String, Any?>> {
+        val blogDto = BlogDto(query, "RECENCY", 1, 6)
         val searchResult = blogService.searchKakao(blogDto)
 
-        val response = mapOf("searchResults" to searchResult)
+        val objectMapper = jacksonObjectMapper()
+        val jsonResult = objectMapper.convertValue(searchResult, Any::class.java)
+
+        val response = mapOf("searchResults" to jsonResult)
+        println(response)
+
         return ResponseEntity.ok(response) // JSON 형식으로 반환
     }
+
 
 }
